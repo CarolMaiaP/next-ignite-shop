7b60c8ceb15c323df7fce0824f3b410a7809398f
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-26 15:53:23 -0300

    styles: home footer atyles added

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -23,4 +23,40 @@ export const Product = styled("a", {
   img: {
     objectFit: 'cover'
   },
+
+  footer: {
+    position: 'absolute',
+    bottom: '0.25rem',
+    left: '0.25rem',
+    right: '0.25rem',
+    padding: '2rem',
+
+    borderRadius: 6,
+
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+
+    transform: 'translateY(110%)',
+    opacity: 0,
+    transition: 'all 0.2s ease-in-out',
+
+    strong: {
+      fontSize: '$lg',
+    },
+
+    span: {
+      fontSize: '$xl',
+      fontWeight: 'bold',
+      color: '$green300',
+    },
+  },
+
+  '&:hover': {
+    footer: {
+      transform: 'translateY(0%)',
+      opacity: 1,
+    }
+  }
 });
