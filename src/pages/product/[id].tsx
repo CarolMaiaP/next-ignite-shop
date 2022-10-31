68edf524333bad74bcb4503c19ab7d1c02d42cbf
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-31 15:51:59 -0300

    feat: internal product page structure and styles added

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores aperiam vel libero doloremque voluptatibus blanditiis aut animi, illum maxime optio voluptatem id exercitationem facere excepturi, temporibus iste inventore totam odit?</p>
+      
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
