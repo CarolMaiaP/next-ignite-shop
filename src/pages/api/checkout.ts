ef47b53593178f57db4cd38bfb89c27a6cab9199
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-11-03 16:20:37 -0300

    feat: return url in stripe checkout

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -18,4 +18,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     ],
   })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
 }
